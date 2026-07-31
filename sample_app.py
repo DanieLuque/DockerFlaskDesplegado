9f7d2f0a15d1e2deb826e642c86f81506fbd6cdc
DanieLuque <Danielcaluque502@gmail.com>
2026-07-31 16:18:05 -0500

    Docker compose

---
+++ sample_app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+import pymysql
+
+sample = Flask(__name__)
+
+@sample.route("/")
+
+def home():
+	try:
+		conn = pymysql.connect(host='servidor-bd-082'
+		, user='root',password='sena123', database='082_db')
+		conn.close()
+		db_status = "Conexión exitosa a la base de datos"
+	except Exception as e:
+		db_status = f"Error al conectar a la base de datos: {e}"
+
+	return f"<h1>Bienvenido a mi aplicación Flask</h1><p>{db_status}</p>"
+
+if __name__ == "__main__":
+	sample.run(host="0.0.0.0", port=5050, debug=True)
+	
